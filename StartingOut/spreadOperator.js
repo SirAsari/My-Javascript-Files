0026d8d6f1cf7c2b3bcbcdae0b236447893dd45b
asari <muhamadasari@smkwikrama.sch.id>
2023-11-16 21:22:38 +0700

    minor changes

---
+++ StartingOut/spreadOperator.js
@@ -0,0 +1,27 @@
+// let user = {
+//     firstName: "Luke",
+//     lastName: "Skywalker",
+//     age: 19,
+//     isJedi: true,
+//     "home world": "Tatooine"
+// }
+
+// let {firstName: Foo, lastName} = user;
+// console.log(Foo);
+
+// let user2 = [
+//     "Luke", "Skywalker", 19, true, "Tatooine"
+// ]
+
+// let [ , lastName] = user2;
+// console.log(lastName);
+
+// const sayHello = () => {
+//     console.log("Selamat pagi semuanya!")
+//   };
+
+const sayHello = (() => {
+    console.log("Selamat pagi semuanya!")
+})
+
+sayHello();
