0d6fe9f6f7b00f92fac86f20681d81cbaa77382e
asari <muhamadasari@smkwikrama.sch.id>
2023-11-19 15:42:42 +0700

    Learning OOP in js, it's kinda lame

---
+++ OOP/basics.js
@@ -0,0 +1,24 @@
+// OOP biasanya diterapkan di Class Based language: C++, C#, Java
+
+const car = {
+    constructor: function(brand, color, maxSpeed, chassisNumber) {
+        this.brand = brand,
+        this.color = color,
+        this.maxSpeed = maxSpeed,
+        this.chassisNumber = chassisNumber
+    },
+    drive: () => {
+      console.log('driving');
+    },
+    reverse: () => {
+      console.log('reversing');
+    },
+    turn: () => {
+      console.log('turning');
+    }
+}
+
+let car1 = Object.create(car);
+car1.constructor('Toyota', 'black', 100, '123456789');  
+console.log(car1.brand);
+
