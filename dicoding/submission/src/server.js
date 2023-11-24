90644bfe547a5ce3c20da87602b0d73315763c8a
asari <muhamadasari@smkwikrama.sch.id>
2023-11-24 10:05:16 +0700

    added dicoding submission files

---
+++ dicoding/submission/src/server.js
@@ -0,0 +1,10 @@
+const Hapi = require("@hapi/hapi");
+const init = async () => {
+  const server = Hapi.server({
+    port: 9000,
+    host: "localhost",
+  });
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+init();
